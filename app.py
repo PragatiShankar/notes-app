941209ff7ef31d9d04152e21d6aae3e2655b0ce7
hi <pragatishankar35@gmail.com>
2026-06-23 23:39:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+notes = [
+    "Learn Dockr",
+    "Build Project",
+    "Deploye App"
+]
+@app.route("/")
+def home():
+    return "Notes API Running!"
+@app.route("/notes")
+def get_notes():
+    return {"notes": notes}
+if __name__ == "__main__":        
+    app.run(host="0.0.0.0", port=5000)
